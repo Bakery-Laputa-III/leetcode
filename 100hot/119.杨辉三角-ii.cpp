ce3b3fcd594e229606ed330f7e3124ed99e03d54
Bakery-Laputa-III <Bakery-Laputa-III@outlook.com>
2025-09-04 11:17:41 +0800

    update

---
+++ 100hot/119.杨辉三角-ii.cpp
@@ -0,0 +1,73 @@
+/*
+ * @lc app=leetcode.cn id=119 lang=cpp
+ *
+ * [119] 杨辉三角 II
+ *
+ * https://leetcode.cn/problems/pascals-triangle-ii/description/
+ *
+ * algorithms
+ * Easy (69.74%)
+ * Likes:    599
+ * Dislikes: 0
+ * Total Accepted:    355.5K
+ * Total Submissions: 509.8K
+ * Testcase Example:  '3'
+ *
+ * 给定一个非负索引 rowIndex，返回「杨辉三角」的第 rowIndex 行。
+ *
+ * 在「杨辉三角」中，每个数是它左上方和右上方的数的和。
+ *
+ *
+ *
+ *
+ *
+ * 示例 1:
+ *
+ *
+ * 输入: rowIndex = 3
+ * 输出: [1,3,3,1]
+ *
+ *
+ * 示例 2:
+ *
+ *
+ * 输入: rowIndex = 0
+ * 输出: [1]
+ *
+ *
+ * 示例 3:
+ *
+ *
+ * 输入: rowIndex = 1
+ * 输出: [1,1]
+ *
+ *
+ *
+ *
+ * 提示:
+ *
+ *
+ * 0
+ *
+ *
+ *
+ *
+ * 进阶：
+ *
+ * 你可以优化你的算法到 O(rowIndex) 空间复杂度吗？
+ *
+ */
+
+// @lc code=start
+class Solution {
+  public:
+    vector<int> getRow(int rowIndex) {
+        vector<int> ans(rowIndex + 1);
+        ans[0] = 1;
+        for (int i = 1; i < rowIndex + 1; i++)
+            ans[i] = (long)ans[i - 1] * (rowIndex - i + 1)
+                     / i; // 乘法(二元都要,防止数据溢出)记得升级然后让他自动降级
+        return ans;
+    }
+};
+// @lc code=end
