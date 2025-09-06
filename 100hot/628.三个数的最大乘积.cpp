f8f0c957a638ba320ad53eb6cf3f40835d06025b
Bakery-Laputa-III <Bakery-Laputa-III@outlook.com>
2025-09-06 21:15:04 +0800

    update

---
+++ 100hot/628.三个数的最大乘积.cpp
@@ -0,0 +1,61 @@
+/*
+ * @lc app=leetcode.cn id=628 lang=cpp
+ *
+ * [628] 三个数的最大乘积
+ *
+ * https://leetcode.cn/problems/maximum-product-of-three-numbers/description/
+ *
+ * algorithms
+ * Easy (51.82%)
+ * Likes:    504
+ * Dislikes: 0
+ * Total Accepted:    148.6K
+ * Total Submissions: 286.7K
+ * Testcase Example:  '[1,2,3]'
+ *
+ * 给你一个整型数组 nums ，在数组中找出由三个数组成的最大乘积，并输出这个乘积。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：nums = [1,2,3]
+ * 输出：6
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：nums = [1,2,3,4]
+ * 输出：24
+ *
+ *
+ * 示例 3：
+ *
+ *
+ * 输入：nums = [-1,-2,-3]
+ * 输出：-6
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 3
+ * -1000
+ *
+ *
+ */
+
+// @lc code=start
+class Solution {
+  public:
+    int maximumProduct(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        return max(nums[0] * nums[1] * nums[nums.size() - 1],
+                   nums[nums.size() - 1] * nums[nums.size() - 2] * nums[nums.size() - 3]);
+    }
+};
+// @lc code=end
